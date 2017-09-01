842ce308aea73c84252be58eac5190c917719d70
dburtnja <dostup.te@gmail.com>
2017-09-01 14:46:24 +0300

    start ft_ls

---
+++ ft_ls.c
@@ -0,0 +1,9 @@
+
+
+#include "ft_ls.h"
+#include "libft/ft_printf/ft_printf.h"
+
+int     main(int argc, char **argv)
+{
+	ft_printf("str%s", " string");
+}
---
+++ ft_ls.h
@@ -0,0 +1,8 @@
+//
+// Created by denys on 01.09.17.
+//
+
+#ifndef FT_LS_FT_LS_H
+#define FT_LS_FT_LS_H
+
+#endif //FT_LS_FT_LS_H

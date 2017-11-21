5df6571bc1787fb372f12e0f117e1063ddb16550
denys <dostup.te@gmail.com>
2017-11-21 21:52:27 +0200

    beta 1.5 simple sort not valide

--- sort_before_print.c
+++ sort_before_print.c
@@ -4,47 +4,47 @@
 
 #include "ft_ls.h"
 
-static int	my_strcmp(const char *s1, const char *s2)
-{
-    size_t  first;
-    size_t  second;
-
-    first = 0;
-    second = 0;
-    while (s1[first] == s2[second] && s1[first] != '\0' && s2[second] != '\0')
-    {
-        first++;
-        second++;
-        while (s1[first] == '.')
-            first++;
-        while (s2[second] == '.')
-            second++;
-    }
-    if (s1[first] - s2[second] == 0)
-        return (int) (first - second);
-    return s1[first] - s2[second];
-}
-
-static int  by_name(t_node *first, t_node *second, int type)
-{
-    char    *first_name;
-    char    *second_name;
-
-    if (type == TYPE_DIR)
-    {
-        first_name = ft_strmap(((t_dir*)first->data)->dir_with_path, &ft_tolower);
-        second_name = ft_strmap(((t_dir*)second->data)->dir_with_path, &ft_tolower);
-    }
-    else if (type == TYPE_FILE)
-    {
-        first_name = ft_strmap(((t_file *) first->data)->file_name, &ft_tolower);
-        second_name = ft_strmap(((t_file *) second->data)->file_name, &ft_tolower);
-    }
-    if (type == TYPE_FILE || type == TYPE_DIR)
-        return my_strcmp(first_name, second_name);
-    else
-        return (0);
-}
+//static int	my_strcmp(const char *s1, const char *s2)
+//{
+//    size_t  first;
+//    size_t  second;
+//
+//    first = 0;
+//    second = 0;
+//    while (s1[first] == s2[second] && s1[first] != '\0' && s2[second] != '\0')
+//    {
+//        first++;
+//        second++;
+//        while (s1[first] == '.')
+//            first++;
+//        while (s2[second] == '.')
+//            second++;
+//    }
+//    if (s1[first] - s2[second] == 0)
+//        return (int) (first - second);
+//    return s1[first] - s2[second];
+//}
+//
+//static int  by_name(t_node *first, t_node *second, int type)
+//{
+//    char    *first_name;
+//    char    *second_name;
+//
+//    if (type == TYPE_DIR)
+//    {
+//        first_name = ft_strmap(((t_dir*)first->data)->dir_with_path, &ft_tolower);
+//        second_name = ft_strmap(((t_dir*)second->data)->dir_with_path, &ft_tolower);
+//    }
+//    else if (type == TYPE_FILE)
+//    {
+//        first_name = ft_strmap(((t_file *) first->data)->file_name, &ft_tolower);
+//        second_name = ft_strmap(((t_file *) second->data)->file_name, &ft_tolower);
+//    }
+//    if (type == TYPE_FILE || type == TYPE_DIR)
+//        return my_strcmp(first_name, second_name);
+//    else
+//        return (0);
+//}
 
 static void select_sort_type(int flags, t_doubly_list *list, int dir)
 {
